2a713983aa596117fbbaf79fcfdd97a3a7170d24
Brent Ford <51456436+bford26@users.noreply.github.com>
2019-11-29 14:29:10 -0500

    Update teapotbase.hh
    Unilat added

--- src/teapot/teapotbase.hh
+++ src/teapot/teapotbase.hh
@@ -74,6 +74,7 @@ namespace teapot_base
                      int nsteps, int useCharge);
 
     void RingRF(Bunch* bunch, double ring_length, int harmonic_numb, double voltage, double phase_s, int useCharge);
+    void UniLat(Bunch* bunch, double length, double LenTunes, double angleX, double angleY, int LatType);
 }
 
 #endif  //TEAPOT_BASE_H
