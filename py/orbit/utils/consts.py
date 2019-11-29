b9e59663c1a34875224ed46e049f182fe2f99902
Brent Ford <51456436+bford26@users.noreply.github.com>
2019-11-29 14:49:42 -0500

    Update consts.py
    added proton classical radius

--- py/orbit/utils/consts.py
+++ py/orbit/utils/consts.py
@@ -5,3 +5,4 @@ mass_proton =  0.938272029 # GeV/c^2
 mass_electron = 0.000511 # GeV/c^2
 mass_electron_mks =  9.10938291e-31 # kg
 charge_electron = 1.602176e-19 # Coulomb
+proton_classical_radius = 1.53469e-18 # m
